e1f3fd42fcfba289f5b8f9b3e820776cc4ae7595
NazarKutoviy <nazarkutoviy@gmail.com>
2024-08-27 13:35:55 +0300

    add

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1
+let result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof (result));
+
+// 2
+let  myEmail = "nazarkutoviy@gmail.com";
+console.log(myEmail.includes("@"));
+console.log(myEmail.length);
+
+// 3
+const my ='my';
+const name ='name';
+const is ='is';
+const message = `${my} ${name} ${is} Nazar`;
+console.log(message);
+
+//4
+let userName = 'alex';
+let paymentCheck = '20$';
+let thanks = `thank's, ${userName} for paying ${paymentCheck}`;
+alert(thanks)
+
